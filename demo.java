894b6d34b24639ae974edbfcb0c23d89ada6ebe2
deeptigit2024 <deeptideshpande.agmcse2023@gmail.com>
2024-09-25 14:41:13 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Student{
+  public static void main(String args[])
+  {
+    int age;
+    String name;
+    public void display()
+      {
+        System.out.println("The name of nthe student is "+name);
+    }
+  }
+}
